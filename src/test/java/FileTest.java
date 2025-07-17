9f81e2c5cc18f19d92e2b165f202901fe6836b2a
Aijan <tilek11.com>
2025-07-17 19:45:58 +0600

    Add new file

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -2,13 +2,14 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class FileTest {
 
     public static void main(String[] args) {
-      
+        File file = new File("Unknown");
     }
 
 }
