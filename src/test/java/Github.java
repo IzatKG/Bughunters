d5615928bb1c0eddc486920d06f7c8d08e1b2099
Bekjan3006 <bekjanergeshov@gmail.com>
2025-07-17 20:29:29 +0600

    Add line

--- src/test/java/Github.java
+++ src/test/java/Github.java
@@ -2,6 +2,7 @@ public class Github {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
         System.out.println("--------------------");
+        System.out.println("Line has been added");
         System.out.println("Work hard, play hard!!!");
     }
 }
