674061dd2e73d546648b851939f409a3d8755332
Bekjan3006 <bekjanergeshov@gmail.com>
2025-07-17 19:42:39 +0600

    Add a new text

--- src/test/java/Github.java
+++ src/test/java/Github.java
@@ -1,5 +1,7 @@
 public class Github {
     public static void main(String[] args) {
         System.out.println("Hello!!!");
+        System.out.println("--------------------");
+        System.out.println("Work hard, play hard!!!");
     }
 }
